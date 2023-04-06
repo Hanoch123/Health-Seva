73628a9f38cb2cfa21cd8d96b4569091c488f11d
Hanoch <65907227+Hanoch123@users.noreply.github.com>
2023-04-06 09:18:58 +0530

    Delete AHIC/Insurance directory

--- AHIC/Insurance/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-from .models import *
-# Register your models here.
-
-admin.site.register(Bank)
-admin.site.register(BankInsurance)
-admin.site.register(UserDetails)
-admin.site.register(Billing)
